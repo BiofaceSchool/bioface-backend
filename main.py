d6c8fcaeade98d5fc1f4ec99eb22933d5eb91bc8
Enzo Trujillo Acosta <enzotrujilloacosta@hotmail.com>
2024-11-02 20:49:48 -0500

    feat: add main

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 from fastapi import FastAPI, HTTPException
+import uvicorn
 from app.Auth.Middlewares.auth_middleware import AuthMiddleware
 from config.dependency_config import create_all_tables
 from app.routes import routes
@@ -13,3 +15,7 @@ except Exception as e:
 app.include_router(routes)
 
 app.add_middleware(AuthMiddleware)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Usa el puerto de entorno o 8000 si no está definido
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
